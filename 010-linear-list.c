17d94850c763fb75fb0de930f2192152d24a7176
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-09-29 23:45:56 +0800

    added algorithm 2.8

--- 010-linear-list.c
+++ 010-linear-list.c
@@ -160,5 +160,15 @@ struct LNode
 // Get the ith element in a single linked list
 Status GetElem_L(LinkList L, int i, ElemType &e)
 {
-    p=
+    p=L->next;
+    j=1;
+    while (p && j<i)
+    {
+        p=p->next;
+        j++;
+    }
+    if (!p || j>i) return ERROR;
+    e=p->data;
+    return OK;
 }
+
