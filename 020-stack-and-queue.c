7cb9dc476ee85de4537a2017f03b986e8982142a
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-10-11 23:37:23 +0800

    to fundamental operation of queue

--- 020-stack-and-queue.c
+++ 020-stack-and-queue.c
@@ -213,3 +213,69 @@ void hanoi(int n, char x, char y, char z)
     }
 }
 
+// Queue
+
+// Linked Queue
+struct QNode
+{
+    QElemType data;
+    struct QNode *next;
+}QNode, *QueuePtr;
+
+struct
+{
+    QueuePtr front;
+    QueuePtr rear;
+}LinkQueue;
+
+Status InitQueue(LinkQueue &Q);
+Status DestroyQueue(LinkQueue &Q);
+Status ClearQueue(LinkQueue &Q);
+Status QueueEmpty(LinkQueue Q);
+int QueueLength(LinkQueue Q);
+Status GetHead(LinkQueue Q, QElemType &e);
+Status EnQueue(LinkQueue &Q, QElemType e);
+Status DeQueue(LinkQueue &Q, QElemType &e);
+Status QueueTraverse(LinkQueue Q, visit());
+
+Status InitQueue(LinkQueue &Q)
+{
+    Q.front=Q.rear=(QueuePtr)malloc(sizeof(QNode));
+    if (!Q.front) return OVERFLOW;
+    Q.front->next=NULL;
+    return OK;
+}
+
+Status DestroyQueue(LinkQueue &Q)
+{
+    while (Q.front)
+    {
+        Q.rear=Q.front->next;
+        free(Q.front);
+        Q.front=Q.rear;
+    }
+    return OK;
+}
+
+Status EnQueue(LinkQueue &Q, QElemType e)
+{
+    p=(QueuePtr)malloc(sizeof(QNode));
+    if (!p) return OVERFLOW;
+    p->data=e;
+    p->next=NULL;
+    Q.rear->next=p;
+    Q.rear=p;
+    return OK;
+}
+
+Status DeQueue(LinkQueue &Q, QElemType &e)
+{
+    if (Q.front==Q.rear) return ERROR;
+    p=Q.front->next;
+    e=p.data;
+    Q.front=p->next;
+    if (Q.rear=p)
+        Q.rear=Q.front;
+    free(p);
+    return OK;
+}
