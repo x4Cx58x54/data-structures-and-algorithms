2150f63e87c980d81202264b7d8aa5abafb5f8e4
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-10-20 23:39:22 +0800

    to def adt hstring halfway

--- 030-string.c
+++ 030-string.c
@@ -66,3 +66,77 @@ Status Concat(SString &T, SString S1, SString S2)
     return uncut;
 } 
 
+// Algorithm 4.3
+Status SubString(SString &Sub, SString S, int pos,int len)
+{
+    if (pos<1 || pos>S[0] || len<0 || len>S[0]-pos+1)
+        return ERROR;
+    Sub[1..len]=S[pos..pos+len-1];
+    Sub[0]=len;
+    return OK;
+}
+
+// Algorithm 4.4
+struct
+{
+    char *ch;
+    int length;
+}HString;
+
+Status StrInsert(HString &S, int pos, HString T)
+{
+    if (pos<1 || pos>S.length+1) return ERROR;
+    if (T.legnth)
+        {
+            if (!(S.ch=(char*)realloc(S.ch,(S.length+T.length)*sizeof(char))))
+                return OVERFLOW;
+            for(i=S.length-1;i>pos;i--)
+                S.ch[i+T.lengh]=S.ch[i];
+            S.ch[pos-1..pos+T.length-2]=T.ch[0..T.length-1];
+            S.length+=T.length;
+        }
+    return OK;
+}
+
+// ADT HString
+Status StrAssign(HString &T, char *chars)
+{
+    if (T.ch) free(T.ch);
+    for(i=0,c=chars;*c;i++,c++);
+    if(!i)
+    {
+        T.ch=NULL;
+        T.length=0;
+    }
+    else
+    {
+        if (!(T.ch=(char*)malloc(i*sizeof(char))))
+            return OVERFLOW;
+        T.ch[0..i-1]=chars[0..i-1];
+        T.length=i;
+    }
+    return OK;
+}
+
+int StrLength(HString)
+{
+    return S.length;
+}
+
+int StrCompare(HString S, HString T)
+{
+    for(i=0;i<S.length && i<T.length;i++)
+        if (S.ch[i]!=T.ch[i]) return S.ch[i]-T.ch[i]
+    return S.length-T.length;
+}
+
+Status ClearString(HString &S)
+{
+    if (S.ch)
+    {
+        free(S.ch);
+        S.ch=NULL;
+    }
+    S.length=0;
+    return OK;
+}
