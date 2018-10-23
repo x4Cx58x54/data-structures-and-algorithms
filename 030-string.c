d3cccfbc064de14282c830c274939f3c4fe6b61f
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-10-23 23:34:38 +0800

    to algo 4.7

--- 030-string.c
+++ 030-string.c
@@ -170,3 +170,83 @@ Status SubString(HString &Sub, HString S, int pos, int len)
     }
     return OK;
 }
+
+// Chunk Link String
+#define CHUNKSIZE 80
+struct Chunk
+{
+    char ch[CHUNKSIZE];
+    struct Chunk *next;
+}Chunk;
+struct
+{
+    Chunk *head, *tail;
+    int curlen;
+}LString;
+
+// Algorithm 4.5
+// Pattern Matching: Simple
+int Index(SString S, SString T, int pos)
+{
+    i=pos;
+    j=1;
+    while(i<=S[0] && j<=T[0])
+    {
+        if (S[i]==T[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            i=i-j+2;
+            j=1;
+        }
+    }
+    if (j>T[0])
+        return i-T[0];
+    else
+        return 0;
+}
+
+// Algorithm 4.6
+// KMP (main)
+int Index_KMP(SString S, SString T, int pos)
+{
+    i=pos;
+    j=1;
+    while (i<=S[0] && j<=T[0])
+    {
+        if (j==0 || S[i]==T[j])
+        {
+            i++;
+            j++;
+        }
+        else
+            j=next(j);
+    }
+    if (j>T[0])
+        return i-T[0];
+    else
+        return 0;
+}
+
+// Algorithm 4.7
+// Next Function
+void get_next(SString T, int next[])
+{
+    i=1;
+    next[1]=0;
+    j=0;
+    while(i<T[0])
+    {
+        if (j==0 || T[i]==T[j])
+        {
+            i++;
+            j++;
+            next[i]=j;
+        }
+        else
+            j=next[j];
+    }
+}
