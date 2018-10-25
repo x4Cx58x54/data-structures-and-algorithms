7e6155fba1a12c7068a6b474e672fea63479f444
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-10-25 23:26:53 +0800

    to algo 4.6

--- 030-string.c
+++ 030-string.c
@@ -184,13 +184,13 @@ struct
     int curlen;
 }LString;
 
-// Algorithm 4.5
-// Pattern Matching: Simple
+// Algorithm 4.6
+// KMP Fast Pattern Matching
 int Index(SString S, SString T, int pos)
 {
     i=pos;
     j=1;
-    while(i<=S[0] && j<=T[0])
+    while ( i<=S[0] && j<=T[0] )
     {
         if (S[i]==T[j])
         {
@@ -198,55 +198,24 @@ int Index(SString S, SString T, int pos)
             j++;
         }
         else
-        {
-            i=i-j+2;
-            j=1;
-        }
+            j=next[j];
     }
     if (j>T[0])
         return i-T[0];
-    else
-        return 0;
-}
-
-// Algorithm 4.6
-// KMP (main)
-int Index_KMP(SString S, SString T, int pos)
-{
-    i=pos;
-    j=1;
-    while (i<=S[0] && j<=T[0])
-    {
-        if (j==0 || S[i]==T[j])
-        {
-            i++;
-            j++;
-        }
-        else
-            j=next(j);
-    }
-    if (j>T[0])
-        return i-T[0];
-    else
-        return 0;
+    return 0;
 }
 
 // Algorithm 4.7
-// Next Function
-void get_next(SString T, int next[])
+// Next function
+int get_next(SString T, int next[])
 {
     i=1;
-    next[1]=0;
     j=0;
-    while(i<T[0])
-    {
-        if (j==0 || T[i]==T[j])
-        {
-            i++;
-            j++;
-            next[i]=j;
-        }
-        else
+    next[1]=0;
+    while (i<T[0])
+        if ( j==0 || T[i]==T[j] )
+            next[++i]=++j;
+        else 
             j=next[j];
-    }
 }
+
