e85a1cf83f5e040108920a1114f4f422cec2c9b7
Li Xiaotian <41692486+AdamMasquerades@users.noreply.github.com>
2018-11-02 21:04:05 +0800

    to algo copybitree

--- 050-tree.c
+++ 050-tree.c
@@ -24,3 +24,105 @@ Status PreOrderTraverse(BiTree T, Status (*Visit)(TElemType e))
     else
         return OK;
 }
+
+// Algorithm 6.2
+// Inorder traversing in stack 1
+Status InOrderTraverse(BiTree T, Status (*Visit)(TElemType e))
+{
+    InitStack(S);
+    Push(S,T);
+    while (!StackEmpty(S))
+    {
+        while (GetTop(S,p)&&p)
+            Push(S, p->lchild); 
+        Pop(S,p);
+        if (!InitStack(S))
+        {
+            Pop(S,p);
+            if(!Visit(p->data)) return ERROR;
+            Push(S,p->rchild);
+        }
+    }
+    return OK;
+}
+
+// Algorithm 6.3
+// Inorder traversing in stack 2
+Status InOrderTraverse(BiTree T, Status (*Visit)(TElemType e))
+{
+    InitStack(S);
+    p=T;
+    while( p || !StackEmpty(S))
+    {
+        if (p) 
+        {
+            Push(S,p);
+            p=p->lchild;
+        }
+        else
+        {
+            Pop(S,p);
+            if(!Visit(p->data)) return ERROR;
+            p=p->rchild;
+        }
+    }
+    return OK;
+}
+
+// Algorithm 6.4
+// PreOrder
+Status CreateBiTree(&T)
+{
+    scanf(&ch);
+    if (ch=='') 
+        T=NULL;
+    else
+    {
+        if (!(T=(BiTNode*)malloc(sizeof(BiTNode)))) return OVERFLOW;
+        T->data=ch;
+        CreateBiTree(T->lchild);
+        CreateBiTree(T->rchild);
+    }
+    return OK;
+}
+
+// Algorithm Extra
+// Calculate the depth of a binary tree by postorder traversing
+int depth(BiTree T)
+{
+    if (T)
+    {
+        ldepth=depth(T->lchild);
+        rdepth=depth(T->rchild);
+        depth= 1+(ldepth>rdepth? ldepth : rdepth);
+    }
+    else
+        depth=0;
+    return depth;
+}
+
+// Algorithm Extra
+// Copy a binary tree
+BiTNode *CreatBiTNode(TElemType e, BiTree ltree, BiTree rtree)
+{
+    if (!(T=(BiTNode*)malloc(sizeof(BiTNode))))
+        return OVERFLOW;
+    T->data=e;
+    T->lchild=ltree;
+    T->rchild=rtree;
+    return T;
+}
+BiTree CopyBiTree(BiTree T)
+{
+    if(!T) return NULL;
+    if (T->lchild) 
+        lp=CopyBiTree(T->lchild);
+    else
+        lp=NULL;
+    if (T->rchild)
+        rp=CopyBiTree(T->rchild);
+    else 
+        rp=NULL;
+    newT=CreatBiTNode(T->data, lp, rp);
+    return newT;
+}
